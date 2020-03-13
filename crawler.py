34551480ee4c38c54095054144dcd03da97cb89d
z7r1k3 <z7r1k3@protonmail.com>
2020-03-13 16:01:50 -0600

    Various display fixes

--- crawler.py
+++ crawler.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-def crawl(totalLevels, levels, passedUrl):
-    totalDepth = int(totalLevels)
-    depth = int(levels)
+def crawl(totalLevels, levels, ogUrl, passedUrl, output):
+    totalDepth = totalLevels
+    depth = levels
 
     if(depth > 0 and not hasCrawled(urlStrip(passedUrl))):
+        if (not '://' in passedUrl and not passedUrl.startswith('/')):
+            passedUrl = 'http://' + passedUrl
         code = requests.get(passedUrl)
         s = BeautifulSoup(code.content, "html.parser")
 
@@ -15,27 +17,26 @@ def crawl(totalLevels, levels, passedUrl):
 
             # If list already exists, append. Else, create
             if (urlStrip(passedUrl) in urlList):
-                urlList[urlStrip(passedUrl)].append(href)
+                if (href not in urlList[urlStrip(passedUrl)]):
+                    urlList[urlStrip(passedUrl)].append(href)
             else:
                 urlList[urlStrip(passedUrl)] = [href]
 
             # Only if domain is not the same as the one passed into this method
-            if ('://' in href):
-                # Add line break at start of new branch
-                if (depth > 1):
-                    print()
-                
+            if ('://' in href):                
                 # Handle formatting
                 for i in range(depth, totalDepth):
                     indent += '     '
                     i
                 
                 # Print domain
-                print(indent + href)
+                display(indent + href, output, totalDepth == depth, depth, ogUrl)
 
                 # Get domains on found domain if depth allows it
-                if (depth > 1):
-                    crawl(totalDepth, depth - 1, href)
+                if (depth > 1 and getDomain(ogUrl) in href):
+                    crawl(totalDepth, depth - 1, ogUrl, href, output)
+        
+        crawlList[urlStrip(passedUrl)] = True
 
     # If URL has already been crawled, use the previously stored URL's
     if (depth > 0 and hasCrawled(urlStrip(passedUrl))):
@@ -43,32 +44,31 @@ def crawl(totalLevels, levels, passedUrl):
             indent = ''
 
             if ('://' in u):
-                # Add line break at start of new branch
-                if (depth > 1):
-                    print()
-
                 # Handle formatting
                 for i in range(depth, totalDepth):
                     indent += '     '
                     i
 
                 # Print domain
-                print(indent + u)
+                display(indent + u, output, totalDepth == depth, depth, ogUrl)
 
                 # Get domains on listed domain if depth allows it
-                if (depth > 1):
-                    crawl(totalDepth, depth - 1, u)
+                if (depth > 1 and getDomain(ogUrl) in u):
+                    crawl(totalDepth, depth - 1, ogUrl, u, output)
+
+        crawlList[urlStrip(passedUrl)] = True
 
 
 def hasCrawled(testUrl):
     check = urlStrip(testUrl)
 
     # Return True if URL has already been crawled
-    if (check in urlList):
+    if (check in crawlList and check in urlList):
         return True
 
     # Return that it has not been crawled
     return False
+
 
 def urlStrip(url):
     bareUrl = url
@@ -85,13 +85,37 @@ def urlStrip(url):
     return bareUrl
 
 
+def display(text, logCode, isRootUrl, depth, ogUrl):
+    switch = {
+        0: isRootUrl,
+        1: True
+    }
+
+    if (switch[logCode] and depth > 1):
+        print()
+
+    if (switch[logCode] and isRootUrl and getDomain(ogUrl) in text):
+        print(text + " | Crawling...")
+    elif (switch[logCode]):
+        print(text)
+
+
+def getDomain(url):
+    index = urlStrip(url).find('/')
+
+    return url[:index]
+
 
 # START MAIN CODE
 
 urlList = {}
+crawlList = {}
 
 # Get user variables
-depth = input("How many levels deep should the crawler go?\n")
 url = input("What is the target URL?\n")
+depth = int(input("How many levels deep should the crawler go?\n"))
+log = int(input("""Please select a logging option:
+0: Display root URL's
+1: Display all URL's\n"""))
 
-crawl(depth, depth, url)
+crawl(depth, depth, url, url, log)
